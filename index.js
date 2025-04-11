85c42918e394a1cc8dfe9366ed30c993f082f460
Yumna-khalid24 <Yumnamaryam.24@yahoo.com>
2025-04-11 11:40:43 +0500

    Alert Added

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("LMAO")
+
+
+console.log("Apologies, sir. I was just practicing your Git and GitHub tutorial. Much respect from Pakistan :)");
+
+alert("Is this your First Ever Open-Source Contribution??")
